f74bdce5637b40046486167fe1043c9e4c62490d
gxz <cnmmail163@163.com>
2023-11-11 16:35:58 +0800

    修复地址

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Typography variant="h2" paragraph>有效流手册</Typography>
       <Typography variant="subtitle2" paragraph>基础社交技能缺失问题标准修复手册</Typography>
       <Box sx={{'& .MuiButton-root': {mx: 1}}}>
-        <Button size="large" variant="contained" href="teaching">开始学习</Button>
+        <Button size="large" variant="contained" href="teach">开始学习</Button>
         <Button size="large" variant="outlined" href="https://github.com/iamnottsh/mdx-template" target="_blank" rel="noreferrer">Github</Button>
       </Box>
     </Box>
